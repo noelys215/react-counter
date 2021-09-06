99158012d344996a2cc956be122218d51eb351ec
noelys215 <69738617+noelys215@users.noreply.github.com>
2021-09-06 11:48:34 -0400

    second commit

---
+++ src/components/RecipeIngredientEdit.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function RecipeIngredientEdit(props) {
+	const { ingredient, handleIngredientChange, handleIngredientDelete } = props;
+
+	function handleChange(changes) {
+		handleIngredientChange(ingredient.id, { ...ingredient, ...changes });
+	}
+
+	return (
+		<>
+			<input
+				className="recipe-edit__input"
+				type="text"
+				onChange={(e) => handleChange({ name: e.target.value })}
+				value={ingredient.name}
+			/>
+			<input
+				className="recipe-edit__input"
+				type="text"
+				onChange={(e) => handleChange({ amount: e.target.value })}
+				value={ingredient.amount}
+			/>
+			<button
+				onClick={() => handleIngredientDelete(ingredient.id)}
+				className="btn btn--danger">
+				&times;
+			</button>
+		</>
+	);
+}
